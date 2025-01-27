e290e6584f18054926e6f3eaddbdfa27edc504c7
Fembunny <bellacyrne@gmail.com>
2025-01-27 17:15:49 -0300

    Mensagem de confirmação

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -4,16 +4,20 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector(".dashboard__item__img");
     let botao = gameClicado.querySelector(".dashboard__item__button");
+    let nomeJogo = gameClicado.querySelector(".dashboard__item__name");
     
     if (imagem.classList.contains("dashboard__item__img--rented")) {
         /* 
         Se o usuario clicar no botao na divisao x (id), e a imagem ter a classe 
         "dashboard__item__img--rented" ficara ofuscada e o botao mudara para "devolver"
         */
-
-        imagem.classList.remove("dashboard__item__img--rented");
-        botao.classList.remove("dashboard__item__button--return");
-        botao.textContent = "Devolver";
+       
+        // Mensagem de confirmação
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove("dashboard__item__img--rented");
+            botao.classList.remove("dashboard__item__button--return");
+            botao.textContent = "Devolver";
+        }
     } else {
         /* 
         Se o usuario clicar no botao na divisao x (id), e a imagem não ter a classe 
