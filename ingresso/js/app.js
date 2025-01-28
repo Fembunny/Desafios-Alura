4fc1a166cc4aa8fa9d1820686290eb966b4397d8
Fembunny <bellacyrne@gmail.com>
2025-01-28 15:33:45 -0300

    Função comprarPista()

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,19 +1,24 @@
 //Função para pegar a opção escolhida pelo usuario e a quantidade de ingressos
 function comprar() {
     let tipo = document.getElementById("tipo-ingresso").value;
-    let qtd = document.getElementById("qtd").value;
+    let qtd = parseInt(document.getElementById("qtd").value);
 
-    console.log(document.getElementById("qtd-pista"));
-    if (qtd > document.getElementById("qtd-pista").value) {
-        console.log("Quantidade de ingressos paraa pista invalida");
+    if (tipo == "pista") {
+        comprarPista(qtd);
+    } else if (tipo == "superior") {
+        //comprarSuperior(qtd);
+    } else {
+        //comprarInferior(qtd);
     }
-    /*
-    let lista = document.getElementsByClassName("lista");
-    console.log(lista);
-    lista.innerHTML = `<li>${tipo}<span id="qtd-pista">${qtd}</span></li>
-                    <li>Cadeira superior<span id="qtd-superior">200</span></li>
-                    <li>Cadeira inferior<span id="qtd-inferior">400</span></li>`;
-    */
-
 }
 
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById("qtd-pista").textContent);
+
+    if (qtd > qtdPista) {
+        alert("Quantidade de ingressos invalida");
+    } else {
+        qtdPista -= qtd;
+        document.getElementById("qtd-pista").textContent = `${qtdPista}`;
+    }
+}
