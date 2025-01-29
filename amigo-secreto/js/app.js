d8295713a8c542da0eddcfed8f75d086ba55356e
Fembunny <bellacyrne@gmail.com>
2025-01-29 15:50:05 -0300

    Função sortear()

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,13 +1,43 @@
+let amigos = [];
+
 // Função para adicionar o nome informado no elemento "lista-amigos"
 function adicionar() {
     let amigo = document.getElementById("nome-amigo");
     let lista = document.getElementById("lista-amigos");
 
-    if (lista.textContent == "") {
-        lista.textContent = amigo.value;
+    if(amigo.value != "") {
+        amigos.push(amigo.value); 
+        lista.textContent = amigos;
     } else {
-        lista.textContent = lista.textContent + ", " + amigo.value;
+        alert("Campo vazio");
     }
 
     document.getElementById("nome-amigo").value = "";
+}
+
+function sortear() {
+    if (amigos != "") {
+        embaralha(amigos);
+        let sorteio = document.getElementById("lista-sorteio");
+
+        for (let i = 0; i < amigos.length; i++) {
+            if (i == amigos.length - 1) {
+                sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+            } else {
+                sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+            }
+        }
+    } else {
+        alert("Lista de amigos vazia");
+    }
+}
+
+// Função responsável por embaralhar o array com os nomes
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista [indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
